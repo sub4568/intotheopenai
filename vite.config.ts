9df89847422ba0949f2158e8239dcb236f1b4052
oluwanifemi odumosu <nifemiodumosu@icloud.com>
2026-01-21 18:50:44 +0100

    configured vite base path and update workflow branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+<<<<<<< HEAD
   server: {
+=======
+base: mode === 'production' ? '/intotheopenai/' : '/',  server: {
+>>>>>>> b1e7252 (configured vite base path and update workflow branch)
     host: "::",
     port: 5174,
     hmr: {
